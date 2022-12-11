2af6978ac2a9569abd958557e96f6cc84b7720ca
remiyamazaki03 <remiyamazaki@gmail.com>
2022-12-11 23:23:50 +0900

    functions & API cleanup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 let apiKey = "8a32636a9f2a4b34c0df70a3a3to1b99";
 let cityInput = document.querySelector("#city");
+let cityDisplay = document.querySelector("#cityDisplay");
+let countryDisplay = document.querySelector("#countryDisplay");
+let weatherDisplay = document.querySelector("#weatherDisplay");
+let iconDisplay = document.querySelector("#weatherDisplay");
+let average = document.querySelector("#average");
+let high = document.querySelector("#high");
+let low = document.querySelector("#low");
 function formatDate() {
   let today = new Date();
   let minute = today.getMinutes().toString().padStart(2, "0");
@@ -23,36 +30,33 @@ function formatDate() {
   let timestamp = document.querySelector("#timeDisplay");
   timestamp.innerHTML = `Last updated: ${month} ${date}, ${hour}:${minute}`;
 }
-function showCity(event) {
+
+function prevent(event) {
   event.preventDefault();
-  document.querySelector("#cityDisplay").innerHTML = cityInput.value;
 }
+
 function handleApi() {
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${cityInput.value}&key=${apiKey}&units=metric`;
   function showTemp(response) {
-    document.querySelector(".average").innerHTML = Math.round(
-      response.data.daily[0].temperature.day
-    );
-    document.querySelector(".high").innerHTML = `${Math.round(
+    average.innerHTML = Math.round(response.data.daily[0].temperature.day);
+    high.innerHTML = `${Math.round(
       response.data.daily[0].temperature.maximum
     )}°C`;
-    document.querySelector(".low").innerHTML = `${Math.round(
+    low.innerHTML = `${Math.round(
       response.data.daily[0].temperature.minimum
     )}°C`;
+    cityDisplay.innerHTML = `${response.data.city}, ${response.data.country}`;
   }
-  axios.get(apiUrl).then(showCity);
   axios.get(apiUrl).then(showTemp);
 }
 function handleApiF(event) {
   event.preventDefault();
   function toFahrenheit(response) {
-    document.querySelector(".average").innerHTML = Math.round(
-      response.data.daily[0].temperature.day
-    );
-    document.querySelector(".high").innerHTML = `${Math.round(
+    average.innerHTML = Math.round(response.data.daily[0].temperature.day);
+    high.innerHTML = `${Math.round(
       response.data.daily[0].temperature.maximum
     )}°F`;
-    document.querySelector(".low").innerHTML = `${Math.round(
+    low.innerHTML = `${Math.round(
       response.data.daily[0].temperature.minimum
     )}°F`;
   }
@@ -63,13 +67,11 @@ function handleApiF(event) {
 function handleApiC(event) {
   event.preventDefault();
   function toCelsius(response) {
-    document.querySelector(".average").innerHTML = Math.round(
-      response.data.daily[0].temperature.day
-    );
-    document.querySelector(".high").innerHTML = `${Math.round(
+    average.innerHTML = Math.round(response.data.daily[0].temperature.day);
+    high.innerHTML = `${Math.round(
       response.data.daily[0].temperature.maximum
     )}°C`;
-    document.querySelector(".low").innerHTML = `${Math.round(
+    low.innerHTML = `${Math.round(
       response.data.daily[0].temperature.minimum
     )}°C`;
   }
@@ -81,31 +83,24 @@ function currentLocation() {
   function inputLocation(position) {
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
-    function showCity(response) {
-      document.querySelector("#cityDisplay").innerHTML = response.data.name;
-    }
     function showTemp(response) {
-      document.querySelector(".average").innerHTML = Math.round(
-        response.data.main.temp
-      );
-      document.querySelector(".high").innerHTML = `${Math.round(
-        response.data.main.temp_max
+      average.innerHTML = Math.round(response.data.daily[0].temperature.day);
+      high.innerHTML = `${Math.round(
+        response.data.daily[0].temperature.maximum
       )}°C`;
-      document.querySelector(".low").innerHTML = `${Math.round(
-        response.data.main.temp_min
+      low.innerHTML = `${Math.round(
+        response.data.daily[0].temperature.minimum
       )}°C`;
+      cityDisplay.innerHTML = `${response.data.city}, ${response.data.country}`;
     }
-    let apiUrlLL = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apikey}&units=metric`;
-    //
-    axios.get(apiUrlLL).then(showCity);
+    let apiUrlLL = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
     axios.get(apiUrlLL).then(showTemp);
   }
   navigator.geolocation.getCurrentPosition(inputLocation);
 }
 formatDate();
+document.querySelector("#enterCity").addEventListener("submit", prevent);
 document.querySelector("#enterCity").addEventListener("submit", handleApi);
-document.querySelector("#enterCity").addEventListener("submit", showCity);
 document.querySelector("#toCelsius").addEventListener("click", handleApiC);
 document.querySelector("#toFahrenheit").addEventListener("click", handleApiF);
 document.querySelector("#current").addEventListener("click", currentLocation);
-//put in #weatherDisplay handling into functions
